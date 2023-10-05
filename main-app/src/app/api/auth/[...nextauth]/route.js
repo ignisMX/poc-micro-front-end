661c309dd7502ffee92096937c6d986c32812604
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-10-05 22:17:29 +0000

    it fixes nextauth configuration

---
+++ main-app/src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,54 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import request from "../../../../services/api";
+const handler = NextAuth({
+  secret: process.env.JWT_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {},
+      async authorize(userAccount, req) {
+        const { username, password } = { ...userAccount };
+        const account = { username, password };
+        const credentials = {
+          client: process.env.JWT_CLIENT,
+          secret: process.env.JWT_SECRET,
+        };
+
+        const response = await request.apiAuthPost(
+          process.env.AUTHENTICATION,
+          credentials,
+          account
+        );
+        return response;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    async jwt({ token, user, account, profile }) {
+      if (user) {
+        token.accessToken = user.access_token;
+        token.refreshToken = user.refresh_token;
+        token.refreshToken = user.refresh_token;
+        token.accessTokenExpires = user.expires_in;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.accessTokenExpires = token.accessTokenExpires;
+      return session;
+    },
+  },
+});
+
+//export default NextAuth(handler);
+
+export { handler as GET, handler as POST };
