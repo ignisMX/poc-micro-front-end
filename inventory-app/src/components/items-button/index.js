8b18abbba61ab68cb6d812dd1d9ad58db639445a
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-10-06 06:06:36 +0000

    it adds component outside app directory

---
+++ inventory-app/src/components/items-button/index.js
@@ -0,0 +1,16 @@
+"use client";
+import { useState } from "react";
+export default function Dashboard({ getItems }) {
+  const [items, setItems] = useState({});
+  const getAllItems = async () => {
+    const response = await getItems();
+    setItems(response);
+  };
+
+  return (
+    <div>
+      {items && JSON.stringify(items)}
+      <button onClick={getAllItems}> Obtener items</button>
+    </div>
+  );
+}
