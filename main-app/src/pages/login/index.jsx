ede0316ae3c4ffeff530156d69cd29b5d23c6ede
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-10-15 20:28:09 +0000

    it adds logic for login

---
+++ main-app/src/pages/login/index.jsx
@@ -0,0 +1,60 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+export default function Login({}) {
+  const router = useRouter();
+  const [userAccount, setUserAccount] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    setUserAccount({
+      ...userAccount,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    const result = await signIn("credentials", {
+      username: userAccount.username,
+      password: userAccount.password,
+      redirect: false,
+    });
+    if (result.ok) {
+      router.push("dashboard");
+    } else {
+    }
+  };
+
+  return (
+    <main>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label>Nombre de usuario </label>
+          <input
+            type="text"
+            name="username"
+            value={userAccount.username}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div>
+          <label>Contraseña </label>
+          <input
+            type="password"
+            name="password"
+            value={userAccount.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    </main>
+  );
+}
