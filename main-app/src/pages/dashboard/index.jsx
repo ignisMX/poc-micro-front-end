92ad266e95670c3bdf91ece80311a098f12d87b8
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2023-10-15 20:27:27 +0000

    it adds logic to dashboard. It is able to download items

--- main-app/src/pages/dashboard/index.jsx
+++ main-app/src/pages/dashboard/index.jsx
@@ -1,3 +1,16 @@
-export default function Dashboard() {
-  return <h1>protected</h1>;
+import { useState } from "react";
+import { findAllItemsAsPage } from "../../services/api-service";
+export default function Dashboard({}) {
+  const [items, setItems] = useState({});
+  const getAllItems = async () => {
+    const response = await findAllItemsAsPage();
+    setItems(response);
+  };
+
+  return (
+    <div>
+      {items && JSON.stringify(items)}
+      <button onClick={getAllItems}> Obtener items</button>
+    </div>
+  );
 }
